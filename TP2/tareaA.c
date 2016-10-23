8bd7a4b5f9fb194f796f1828f7614bd2f7d42fb2
francodi65 <francodi65@gmail.com>
2016-10-23 01:08:22 -0300

    Busqueda recursiva

--- TP2/tareaA.c
+++ TP2/tareaA.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
 #define BOLDCYAN    "\033[1m\033[36m"
@@ -10,6 +12,7 @@
 
 int leerEntrada(char* argv[], char* command);
 int getPaths(char* paths[]);
+int searchFile(char* path,int recursive);
 
 int main (){
 		char command[20];
@@ -66,4 +69,57 @@ int getPaths(char* paths[]){
 	return pathCounter+1;
 }
 
+/**
+ * Busca el archivo dentro del path y redirecciona el puntero enviado.
+ * @param char* path Ruta + archivo a buscar. Ej "/bin/date"
+ * @param int recursive Enviar un 0 para busqueda local y un 1 para busqueda recursiva
+ * @return Devuelve 1 si encontro el archivo y un 0 si no lo encontro.
+ */
+
+int searchFile(char* path,int recursive){
+	DIR * pDir;
+	char* arch;
+	char auxPath[50];
+
+	arch = strchr(path, '\0');							//Con este algoritmo obtengo el nombre
+	while (*arch != '/')								    //del archivo al que quiero acceder
+		arch--;											          //y obtengo la ruta por otro lado.
+	*arch= '\0';
+	arch++;
+	strcpy(auxPath,arch);
+
+  pDir = opendir (path);
+
+  if (! pDir) {
+      printf ("Error al abrir el directorio %s",path);
+      return 0;
+  }
+
+  while (1) {
+      struct dirent * file;
+      file = readdir (pDir);
+      if (! file){
+          break;
+      }else if(!strcmp(file->d_name,arch)){             //Chequeo si esta en esa carpeta
+      	strcat(path,"/");
+      	strcat(path,file->d_name);
+      	return 1;
+      } else if (file->d_type == DT_DIR && recursive && strcmp(file->d_name,"..") && strcmp(file->d_name,".") 
+      			&& strchr(file->d_name,'.')==NULL){        //Si esta habilitada la busqueda recursiva, busco en otras carpetas
+      	strcat(path,"/");
+      	strcat(path,file->d_name);
+      	strcat(path,"/");
+      	strcat(path,auxPath);
+      	if (searchFile(path,recursive)){                 // Si lo encontro, retorno 1
+      		return 1;
+      	} else {                                      //Sino, elimino la carpeta que agregue, y vuelvo a buscar
+      		char* p = strchr(path, '\0');							
+				  while (*p != '/')
+            p--;										
+				  *p= '\0';
+      	}   
+      }
+  }
+	return 0;
+}
 						
