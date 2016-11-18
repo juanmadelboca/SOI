80e58ff26ccea96b26ef03eab24a2710c42feb8b
francodi65 <francodi65@gmail.com>
2016-11-18 20:32:20 -0300

    elimino tmp

--- TP3/.encrip.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/franco/SOI/TP3/encrip.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/franco/SOI/TP3/encrip.ko /home/franco/SOI/TP3/encrip.o /home/franco/SOI/TP3/encrip.mod.o
--- TP3/.tmp_versions/encrip.mod
+++
@@ -1,2 +0,0 @@
-/home/franco/SOI/TP3/encrip.ko
-/home/franco/SOI/TP3/encrip.o
