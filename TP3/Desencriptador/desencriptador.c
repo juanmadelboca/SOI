d5bb227d11d2e56b448e0cf7d497c586da2650b0
juanmadelboca <juanmadelboca@gmail.com>
2016-11-19 15:47:18 -0300

    subo traduccion de los comentarios

--- TP3/Desencriptador/desencriptador.c
+++ TP3/Desencriptador/desencriptador.c
@@ -1,38 +1,30 @@
 /*
- *  chardev.c - Create an input/output character device
+ *  Desencriptador desencripta un mensaje escrito en su dev file
  */
 
-#include <linux/kernel.h>	/* We're doing kernel work */
-#include <linux/module.h>	/* Specifically, a module */
+#include <linux/kernel.h>	
+#include <linux/module.h>	
 #include <linux/fs.h>
-#include <asm/uaccess.h>	/* for get_user and put_user */
-
+#include <asm/uaccess.h>	
 #include "desencriptador.h"
 #define SUCCESS 0
 #define DEVICE_NAME "desencrip"
 #define BUF_LEN 80
 
 /* 
- * Is the device open right now? Used to prevent
- * concurent access into the same device 
+ * Para evitar abrir 2 veces el archivo
  */
 static int Device_Open = 0;
-/* 
- * The message the device will give when asked 
- */
+
 static char Message[BUF_LEN];
 
  /*
- * The message the device will give when asked 
- 
- * How far did the process reading the message get?
- * Useful if the message is larger than the size of the
- * buffer we get to fill in device_read. 
+ * puntero para leer mensaje
  */
 static char *Message_Ptr;
 
 /* 
- * This is called whenever a process attempts to open the device file 
+ * cuando un proceso quiere leer el device llama a esta funcion
  */
 static int device_open(struct inode *inode, struct file *file)
 {
@@ -40,16 +32,12 @@ static int device_open(struct inode *inode, struct file *file)
 	printk(KERN_INFO "device_open(%p)\n", file);
 #endif
 
-	/* 
-	 * We don't want to talk to two processes at the same time 
-	 */
+	
 	if (Device_Open)
 		return -EBUSY;
 
 	Device_Open++;
-	/*
-	 * Initialize the message 
-	 */
+	
 	Message_Ptr = Message;
 	try_module_get(THIS_MODULE);
 	return SUCCESS;
@@ -61,9 +49,7 @@ static int device_release(struct inode *inode, struct file *file)
 	printk(KERN_INFO "device_release(%p,%p)\n", inode, file);
 #endif
 
-	/* 
-	 * We're now ready for our next caller 
-	 */
+	
 	Device_Open--;
 
 	module_put(THIS_MODULE);
@@ -71,43 +57,29 @@ static int device_release(struct inode *inode, struct file *file)
 }
 
 /* 
- * This function is called whenever a process which has already opened the
- * device file attempts to read from it.
+ *Se llama a esta funcion cuando un proceso abrio el dev y quiere leerlo
  */
-static ssize_t device_read(struct file *file,	/* see include/linux/fs.h   */
-			   char __user * buffer,	/* buffer to be
-							 * filled with data */
-			   size_t length,	/* length of the buffer     */
+static ssize_t device_read(struct file *file,	
+			   char __user * buffer,	
+							 
+			   size_t length,	
 			   loff_t * offset)
 {
-	/* 
-	 * Number of bytes actually written to the buffer 
-	 */
+	
 	int bytes_read = 0;
 
 #ifdef DEBUG
 	printk(KERN_INFO "device_read(%p,%p,%d)\n", file, buffer, length);
 #endif
 
-	/* 
-	 * If we're at the end of the message, return 0
-	 * (which signifies end of file) 
-	 */
+	
 	if (*Message_Ptr == 0)
 		return 0;
 
-	/* 
-	 * Actually put the data into the buffer 
-	 */
+	
 	while (length && *Message_Ptr) {
 
-		/* 
-		 * Because the buffer is in the user data segment,
-		 * not the kernel data segment, assignment wouldn't
-		 * work. Instead, we have to use put_user which
-		 * copies data from the kernel data segment to the
-		 * user data segment. 
-		 */
+		
 		put_user(*(Message_Ptr++), buffer++);
 		length--;
 		bytes_read++;
@@ -118,16 +90,12 @@ static ssize_t device_read(struct file *file,	/* see include/linux/fs.h   */
 	printk(KERN_INFO "Read %d bytes, %d left\n", bytes_read, length);
 #endif
 
-	/* 
-	 * Read functions are supposed to return the number
-	 * of bytes actually inserted into the buffer 
-	 */
+	
 	return bytes_read;
 }
 
 /* 
- * This function is called when somebody tries to
- * write into our device file. 
+ *Funcion llamada cuando alguien quiere escribir el dev. 
  */
 static ssize_t
 device_write(struct file *file,
@@ -147,45 +115,27 @@ device_write(struct file *file,
 
 	Message_Ptr = Message;
 
-	/* 
-	 * Again, return the number of input characters used 
-	 */
+	
 	return i;
 }
 
 /* 
- * This function is called whenever a process tries to do an ioctl on our
- * device file. We get two extra parameters (additional to the inode and file
- * structures, which all device functions get): the number of the ioctl called
- * and the parameter given to the ioctl function.
- *
- * If the ioctl is write or read/write (meaning output is returned to the
- * calling process), the ioctl call returns the output of this function.
- *
+ *esta funcion es llamada cuando el modulo necesita a ioctl ya sea para escribir o leer el dev
  */
-long device_ioctl(struct file *file,	/* ditto */
-		 unsigned int ioctl_num,	/* number and param for ioctl */
+long device_ioctl(struct file *file,	
+		 unsigned int ioctl_num,	
 		 unsigned long ioctl_param)
 {
 	int i;
 	char *temp;
 	char ch;
 
-	/* 
-	 * Switch according to the ioctl called 
-	 */
+	
 	switch (ioctl_num) {
 	case IOCTL_SET_MSG:
-		/* 
-		 * Receive a pointer to a message (in user space) and set that
-		 * to be the device's message.  Get the parameter given to 
-		 * ioctl by the process. 
-		 */
+		
 		temp = (char *)ioctl_param;
 
-		/* 
-		 * Find the length of the message 
-		 */
 		get_user(ch, temp);
 		for (i = 0; ch && i < BUF_LEN; i++, temp++)
 			get_user(ch, temp);
@@ -194,24 +144,14 @@ long device_ioctl(struct file *file,	/* ditto */
 		break;
 
 	case IOCTL_GET_MSG:
-		/* 
-		 * Give the current message to the calling process - 
-		 * the parameter we got is a pointer, fill it. 
-		 */
+		
 		i = device_read(file, (char *)ioctl_param, 99, 0);
 
-		/* 
-		 * Put a zero at the end of the buffer, so it will be 
-		 * properly terminated 
-		 */
 		put_user('\0', (char *)ioctl_param + i);
 		break;
 
 	case IOCTL_GET_NTH_BYTE:
-		/* 
-		 * This ioctl is both input (ioctl_param) and 
-		 * output (the return value of this function) 
-		 */
+		
 		return Message[ioctl_param];
 		break;
 	}
@@ -222,34 +162,25 @@ long device_ioctl(struct file *file,	/* ditto */
 /* Module Declarations */
 
 /* 
- * This structure will hold the functions to be called
- * when a process does something to the device we
- * created. Since a pointer to this structure is kept in
- * the devices table, it can't be local to
- * init_module. NULL is for unimplemented functions. 
+ * Esta estructura contiene las funciones que seran llamadas cuando un proceso haga algo al device que creamos
+ * un puntero a esta estructura es guardado en la tabla de devices
  */
 struct file_operations Fops = {
 	.read = device_read,
 	.write = device_write,
 	.unlocked_ioctl = device_ioctl,
 	.open = device_open,
-	.release = device_release,	/* a.k.a. close */
+	.release = device_release,	
 };
 
 /* 
- * Initialize the module - Register the character device 
+ * Inicializa el modulo y lo registra
  */
 int init_module()
 {
 	int ret_val;
-	/* 
-	 * Register the character device (atleast try) 
-	 */
 	ret_val = register_chrdev(MAJOR_NUM, DEVICE_NAME, &Fops);
 
-	/* 
-	 * Negative values signify an error 
-	 */
 	if (ret_val < 0) {
 		printk(KERN_ALERT "%s failed with %d\n",
 		       "Sorry, registering the character device ", ret_val);
@@ -270,13 +201,11 @@ int init_module()
 }
 
 /* 
- * Cleanup - unregister the appropriate file from /proc 
+ * Saca el modulo, lo desregistra 
  */
 void cleanup_module()
 {
-	/* 
-	 * Unregister the device 
-	 */
+	
 	unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
 
 }
